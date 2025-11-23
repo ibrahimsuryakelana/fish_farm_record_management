1ccf70c8ee0ac0bc8c7a81e27c811b8013e4d3e0
rayhanfirmansyah29 <01086230011@student.uph.edu>
2025-11-23 19:41:09 +0700

    CRUD Pakan

--- database/migrations/2025_11_05_074122_create_pakans_table.php
+++ database/migrations/2025_11_05_074122_create_pakans_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    public function up(): void
+    public function up(): void //test
     {
         Schema::create('pakans', function (Blueprint $table) {
             $table->id('id_pakan');
